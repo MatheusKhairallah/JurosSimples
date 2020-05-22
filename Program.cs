f1b12f80cb76940cabbc567adc5efe4ddb8808b4
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-05-22 20:09:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcule o montante final de um investimento a juros simples.");
+            double CC=1200;
+            double I=2;
+            double T=15;
+            double resultado=CC * I;
+            double resultado_1=resultado / 100;
+            double resultado_2=resultado_1 * T;
+            double J=resultado_2;
+            double resultado2=CC + J;
+            Console.WriteLine($"{resultado_2:C}");
+            Console.WriteLine($"{resultado2:C}");
+        }
+    }
+}
